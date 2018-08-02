93fc3984f4d6553299ada0d7bdfebe8d1cf5e8e7
Germán Escobar <german.escobarc@gmail.com>
2018-08-02 11:42:44 -0400

    Fix POST route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded());
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
@@ -9,7 +10,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  res.send("<h1>Hola</h1>");
+  res.send(`<h1>Hola ${req.body.name}!</h1>`);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
